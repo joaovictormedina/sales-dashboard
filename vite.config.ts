00fd5920cf41e94baa90592efeefb9b863bfaa37
João Victor Medina <joaovictor.zipp@gmail.com>
2024-10-04 15:54:06 -0300

    corrigindo conflito no vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Certifique-se de que só esta importação está presente
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      '@': path.resolve(__dirname, './src'), // Resolvendo o caminho da pasta src
+    },
+  },
 })
